3a57fe433e930d1d758084c6ffac345db76d8e1d
unknown <matthiascrespo9@gmail.com>
2024-01-10 14:39:36 +0100

    suppr console.log + add lecture partager

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./src/config/db'); 
+const http = require('http');
+const { Server } = require('socket.io');
+const connectDB = require('./src/config/db');
+
+// Importation des routes
 const musicRoutes = require('./src/routes/musicRoutes'); 
 const artistRoutes = require('./src/routes/artistRoutes');
 const albumRoutes = require('./src/routes/albumRoutes');
@@ -11,6 +15,13 @@ const searchRoutes = require('./src/routes/searchRoutes');
 connectDB();
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: process.env.FRONTEND_URL, // Remplacez par l'URL de votre front-end
+        methods: ["GET", "POST"]
+    }
+});
 
 // Middleware pour le JSON
 app.use(express.json());
@@ -25,5 +36,40 @@ app.use('/api/album', albumRoutes);
 app.use('/api/playlist', playlistRoutes);
 app.use('/api/search', searchRoutes);
 
+// État global de la lecture
+let currentTrack = null; // L'ID ou les détails de la musique actuellement jouée
+let playbackState = { currentTime: 0 }; // L'état de lecture actuel (position de la musique)
+
+// Gestion des connexions WebSocket
+io.on('connection', (socket) => {
+
+    // Envoyer l'état actuel de la musique et de lecture au client qui vient de se connecter
+    if (currentTrack) {
+        socket.emit('musicChange', currentTrack);
+    }
+    socket.emit('playbackUpdate', playbackState);
+
+    socket.on('play', () => {
+        io.emit('play');
+    });
+
+    socket.on('pause', () => {
+        io.emit('pause');
+    });
+
+    socket.on('changeMusic', (track) => {
+        currentTrack = track; // Mettre à jour la musique actuellement jouée
+        io.emit('musicChange', currentTrack); // Informer tous les clients
+    });
+
+    socket.on('updatePlayback', (state) => {
+        playbackState = state;
+        socket.broadcast.emit('playbackUpdate', playbackState); // Informer les autres clients
+    });
+
+    socket.on('disconnect', () => {
+    });
+});
+
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT);
--- src/config/db.js
+++ src/config/db.js
@@ -9,7 +9,6 @@ const connectDB = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
-        console.log('MongoDB Connected...');
     } catch (err) {
         console.error(err.message);
         process.exit(1);
